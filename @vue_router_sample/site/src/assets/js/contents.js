24d4b355ec359f265cb1ce76a2ece811b92dd1fd
august5pm <august5pm@gmail.com>
2023-01-13 15:09:30 +0900

    Delete @vue_router_sample directory
    폴더정리

--- @vue_router_sample/site/src/assets/js/contents.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-var frame = require('./frame_setting');
-
-frame.preset.json_url = window.GlobalPreset.JSON_URL.GET_DATA_CONTENTS;
-frame.mainContent.data = require("./interactive/contents/contents-setting.vue");
-
-new Vue(frame);
